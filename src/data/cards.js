8a3a2e26ade5d6f57e1270d18e8612d2e5c62c04
Silva Filho <tarusilva@gmail.com>
2022-11-03 22:44:34 -0300

    feat: cria o array de objetos de cartas

---
+++ src/data/cards.js
@@ -0,0 +1,87 @@
+// @ts-ignore
+import Heroku from "../assets/heroku.png";
+// @ts-ignore
+import HtmlCss from "../assets/htmlcss.png";
+// @ts-ignore
+import JavaScript from "../assets/javascript.png";
+// @ts-ignore
+import Netlify from "../assets/netlify.png";
+// @ts-ignore
+import NodeJS from "../assets/nodejs.png";
+// @ts-ignore
+import ReactJS from "../assets/react.png";
+
+export const cards = [
+    {
+        id: 1,
+        name: "Heroku",
+        image: Heroku,
+        turned: false
+    },
+    {
+        id: 2,
+        name: "HTML & CSS",
+        image: HtmlCss,
+        turned: false
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        image: JavaScript,
+        turned: false
+    },
+    {
+        id: 4,
+        name: "Netlify",
+        image: Netlify,
+        turned: false
+    },
+    {
+        id: 5,
+        name: "NodeJS",
+        image: NodeJS,
+        turned: false
+    },
+    {
+        id: 6,
+        name: "ReactJS",
+        image: ReactJS,
+        turned: false
+    },
+    {
+        id: 7,
+        name: "Heroku",
+        image: Heroku,
+        turned: false
+    },
+    {
+        id: 8,
+        name: "HTML & CSS",
+        image: HtmlCss,
+        turned: false
+    },
+    {
+        id: 9,
+        name: "JavaScript",
+        image: JavaScript,
+        turned: false
+    },
+    {
+        id: 10,
+        name: "Netlify",
+        image: Netlify,
+        turned: false
+    },
+    {
+        id: 11,
+        name: "NodeJS",
+        image: NodeJS,
+        turned: false
+    },
+    {
+        id: 12,
+        name: "ReactJS",
+        image: ReactJS,
+        turned: false
+    }
+];
