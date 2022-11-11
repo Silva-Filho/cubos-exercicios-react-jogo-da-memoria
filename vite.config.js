6d75703b537ccae6670dd265bd2804e0d65f3b03
Silva Filho <tarusilva@gmail.com>
2022-11-10 22:26:46 -0300

    refactor: adiciona configurações para usar Vitest
    Adiciona as configurações para usar o Vitest.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig( {
-    plugins: [ react() ]
+    plugins: [ react() ],
+    // @ts-ignore
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: [ "./src/setupTests.js" ],
+        // you might want to disable it, if you don't have tests that rely on CSS
+        // since parsing CSS is slow
+        css: true,
+        include: [ "**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}" ],
+        exclude: [ "node_modules", "dist", ".idea", ".git", ".cache" ],
+        coverage: {
+            reporter: [ "text", "json", "html" ],
+        }
+    },
 } );
