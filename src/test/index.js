09ba1d1cc6bca9b55714e71c379af4305afae633
Silva Filho <tarusilva@gmail.com>
2022-11-10 22:31:27 -0300

    feat: cria arquivo de importação do métodos de teste
    Cria arquivo para centralizar as importações dos métodos usados
    nos testes.

---
+++ src/test/index.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it, vi } from "vitest";
+
+export { 
+    describe, 
+    expect, 
+    it, 
+    render, 
+    screen, 
+    userEvent, 
+    vi, 
+};
