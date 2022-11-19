0c2dc25e6691f8b4a9f6459631b2d5af4004137c
Silva Filho <tarusilva@gmail.com>
2022-11-18 21:32:09 -0300

    feat: cria testes para o Sidebar
    Cria os seguintes testes:
    
    - testa se o clique com o botão esquerdo do mouse no botão Reset
      dispara a função handleReset;
    
    - teste se ao usuário usar a tecla tab o botão reset recebe focus; e
    
    - teste se ao usuário usar a tecla Enter quando o botão Reset estiver
      em foco dispara a função handleReset.

---
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { describe, expect, it, render, screen, userEvent, vi } from "../../test/index";
+
+import { Sidebar } from "./index";
+import { cards } from "../../data/cards";
+// screen.debug();
+// screen.logTestingPlaygroundURL();
+describe( "component Sidebar", () => {
+    it( "should call function setDeck when button Reset is clicked", async () => {
+        const sortedCards = cards;
+        const setDeck = vi.fn();
+
+        render(
+            <Sidebar
+                cards={ sortedCards }
+                setDeck={ setDeck }
+            />
+        );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+        await userEvent.click( buttonReset );
+
+        expect( setDeck ).toHaveBeenCalledOnce();
+        expect( setDeck ).toHaveBeenCalledTimes( 1 );
+    } );
+
+    it( "should focus Reset button when tab", async () => {
+        const sortedCards = cards;
+        const setDeck = vi.fn();
+
+        render(
+            <Sidebar
+                cards={ sortedCards }
+                setDeck={ setDeck }
+            />
+        );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+        await userEvent.tab();
+
+        expect( buttonReset ).toHaveFocus();
+    } );
+
+    it( "should call function setDeck when press key Enter on button Reset focused ", async () => {
+        const sortedCards = cards;
+        const setDeck = vi.fn();
+
+        render(
+            <Sidebar
+                cards={ sortedCards }
+                setDeck={ setDeck }
+            />
+        );
+        
+        await userEvent.tab();
+        await userEvent.keyboard( "[Enter]" );
+
+        expect( setDeck ).toHaveBeenCalledOnce();
+        expect( setDeck ).toHaveBeenCalledTimes( 1 );
+    } );
+} );
