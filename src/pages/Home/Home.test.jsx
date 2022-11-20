74ba6200207f77fd65e910a0bbf7e469d7065535
Silva Filho <tarusilva@gmail.com>
2022-11-19 22:09:42 -0300

    feat: cria testes que simula ações do usuário
    Cria os seguintes testes para simular a interação do usuário:
    
    - quando o usuário tenta virar mais de duas cartas, a terceira carta
      não deve ser virada;
    
    - quando o usuário virar a mesma carta duas (mesmo ID), esta não
      deve impedir de uma nova carta ser virada;
    
    - quando o usuário virar duas cartas diferentes, elas devem ser
      automaticamente desviradas;
    
    - quando o usuário virar duas cartas iguais, elas devem ser
      automaticamente removidas do display; e
    
    - quando o usuário virar corretamente todos os pares, deve aparecer
      uma imagem de congratulação em tela.

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,4 +1,4 @@
-import { cleanup, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
+import { cleanup, findByRole, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import React from "react";
 
 import { describe, expect, it, render, screen, userEvent, vi } from "../../test";
@@ -53,4 +53,246 @@ describe( "page Home - layout tests", () => {
 
         expect( screen.getAllByAltText( /ReactJS/i ) ).toHaveLength( 2 );
     } );
+} );
+// screen.logTestingPlaygroundURL();
+describe( "page Home - functions tests", () => {
+    it( "should not accept more than two card is turned", async () => {
+        render( <Home /> );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+
+        await waitFor( () => userEvent.click( buttonReset ) );
+
+        const cardHtmlCss = screen.getByTestId( "2" );
+        const cardNodeJs = screen.getByTestId( "5" );
+        const cardReactJs = screen.getByTestId( "6" );
+
+        await userEvent.click( cardHtmlCss );
+        await userEvent.click( cardNodeJs );
+        await userEvent.click( cardReactJs );
+
+        expect( cardHtmlCss ).not.toHaveAttribute(
+            "src",
+            expect.stringMatching( /card-back/i )
+        );
+        expect( cardNodeJs ).not.toHaveAttribute(
+            "src",
+            expect.stringMatching( /card-back/i )
+        );
+        expect( cardReactJs ).toHaveAttribute(
+            "src",
+            expect.stringMatching( /card-back/i )
+        );
+    } );
+
+    it( "should do nothing when the same card is clicked twice", async () => {
+        render( <Home /> );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+
+        await waitFor( () => userEvent.click( buttonReset ) );
+
+        const cardHtmlCss = screen.getByTestId( "2" );
+
+        await userEvent.click( cardHtmlCss );
+        await userEvent.click( cardHtmlCss );
+
+        expect( cardHtmlCss ).toHaveAttribute(
+            "src",
+            expect.stringMatching( /card-back/i )
+        );
+    } );
+
+    it( "should turned off when de two cards are diferents", async () => {
+        render( <Home /> );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+
+        await waitFor( () => userEvent.click( buttonReset ) );
+
+        const cardHtmlCss = screen.getByTestId( "2" );
+        const cardNodeJs = screen.getByTestId( "5" );
+
+        await userEvent.click( cardHtmlCss );
+        await userEvent.click( cardNodeJs );
+
+        await waitFor( () => {
+            expect( cardHtmlCss ).toHaveAttribute(
+                "src",
+                expect.stringMatching( /card-back/i )
+            );
+            expect( cardNodeJs ).toHaveAttribute(
+                "src",
+                expect.stringMatching( /card-back/i )
+            );
+        } );
+    } );
+
+    it( "should remove card '/html & css/i' when founded the pair", async () => {
+        render( <Home /> );
+
+        const [ cardsHtmlCss01, cardsHtmlCss02 ] = screen.getAllByRole( "img", { name: /html & css/i } );
+
+        await userEvent.click( cardsHtmlCss01 );
+        await userEvent.click( cardsHtmlCss02 );
+
+        await waitFor( () => {
+            expect( cardsHtmlCss01 ).not.toBeInTheDocument();
+            expect( cardsHtmlCss02 ).not.toBeInTheDocument();/*  */
+        } );
+        /* await waitForElementToBeRemoved( [ cardsHtmlCss01, cardsHtmlCss02 ] );
+        expect( cardsHtmlCss01 ).not.toBeInTheDocument();
+        expect( cardsHtmlCss02 ).not.toBeInTheDocument(); */
+    } );
+
+    it( "should display 'Congrats' message when the game is finished", async () => {
+        render( <Home /> );
+
+        const buttonReset = screen.getByRole( "button", { name: /reset/i } );
+
+        await waitFor( () => userEvent.click( buttonReset ) );
+        
+        const [ cardsHeroku01, cardsHeroku02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /heroku/i
+            }
+        );
+
+        await userEvent.click( cardsHeroku01 );
+        await userEvent.click( cardsHeroku02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsHeroku01,
+                cardsHeroku02
+            ]
+        );
+        expect( cardsHeroku01 ).not.toBeInTheDocument();
+        expect( cardsHeroku02 ).not.toBeInTheDocument();
+
+        const [ cardsHtmlCss01, cardsHtmlCss02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /html & css/i
+            }
+        );
+
+        await userEvent.click( cardsHtmlCss01 );
+        await userEvent.click( cardsHtmlCss02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsHtmlCss01,
+                cardsHtmlCss02
+            ]
+        );
+        expect( cardsHtmlCss01 ).not.toBeInTheDocument();
+        expect( cardsHtmlCss02 ).not.toBeInTheDocument();
+
+        const [ cardsJavaScript01, cardsJavaScript02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /javascript/i
+            }
+        );
+
+        await userEvent.click( cardsJavaScript01 );
+        await userEvent.click( cardsJavaScript02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsJavaScript01,
+                cardsJavaScript02
+            ]
+        );
+        expect( cardsJavaScript01 ).not.toBeInTheDocument();
+        expect( cardsJavaScript02 ).not.toBeInTheDocument();
+
+        const [ cardsNetlify01, cardsNetlify02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /Netlify/i
+            }
+        );
+
+        await userEvent.click( cardsNetlify01 );
+        await userEvent.click( cardsNetlify02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsNetlify01,
+                cardsNetlify02
+            ]
+        );
+        expect( cardsNetlify01 ).not.toBeInTheDocument();
+        expect( cardsNetlify02 ).not.toBeInTheDocument();
+
+        const [ cardsNodeJS01, cardsNodeJS02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /NodeJS/i
+            }
+        );
+
+        await userEvent.click( cardsNodeJS01 );
+        await userEvent.click( cardsNodeJS02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsNodeJS01,
+                cardsNodeJS02
+            ]
+        );
+        expect( cardsNodeJS01 ).not.toBeInTheDocument();
+        expect( cardsNodeJS02 ).not.toBeInTheDocument();
+
+        const [ cardsReactJS01, cardsReactJS02 ] = screen.getAllByRole(
+            "img",
+            {
+                name: /ReactJS/i
+            }
+        );
+
+        await userEvent.click( cardsReactJS01 );
+        await userEvent.click( cardsReactJS02 );
+
+        await waitForElementToBeRemoved(
+            [
+                cardsReactJS01,
+                cardsReactJS02
+            ]
+        );
+        expect( cardsReactJS01 ).not.toBeInTheDocument();
+        expect( cardsReactJS02 ).not.toBeInTheDocument();
+
+        const congratsImg = await screen.findByLabelText( /congrats/i );
+
+        expect( congratsImg ).toBeInTheDocument();
+
+        /* await waitFor( () => {
+            expect( cardsHeroku01 ).not.toBeInTheDocument();
+            expect( cardsHeroku02 ).not.toBeInTheDocument();
+            expect( cardsHtmlCss01 ).not.toBeInTheDocument();
+            expect( cardsHtmlCss02 ).not.toBeInTheDocument();
+            expect( cardsJavaScript01 ).not.toBeInTheDocument();
+            expect( cardsJavaScript02 ).not.toBeInTheDocument();
+        } ); */
+        /* await waitForElementToBeRemoved( 
+            [ 
+                cardsHeroku01, 
+                cardsHeroku02, 
+                cardsHtmlCss01, 
+                cardsHtmlCss02, 
+                cardsJavaScript01, 
+                cardsJavaScript02, 
+            ] 
+        );
+        expect( cardsHeroku01 ).not.toBeInTheDocument();
+        expect( cardsHeroku02 ).not.toBeInTheDocument();
+        expect( cardsHtmlCss01 ).not.toBeInTheDocument();
+        expect( cardsHtmlCss02 ).not.toBeInTheDocument();
+        expect( cardsJavaScript01 ).not.toBeInTheDocument();
+        expect( cardsJavaScript02 ).not.toBeInTheDocument(); */
+    }, 7000 );
 } );
