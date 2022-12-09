839dc8516cf72b3e7474e8aa901ffbc745fdad77
Silva Filho <tarusilva@gmail.com>
2022-12-08 23:43:26 -0300

    refactor: refatora Sidebar e exclui código repetido
    - Cria a função customRender para remover a repetição do render; e
    
    - Usa vi.clearAllMocks() no afterEach para garantir que as
      informações de um teste não interferi no outro.

--- src/components/Sidebar/Sidebar.test.jsx
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -1,67 +1,68 @@
 import React from "react";
 
-import { 
-    describe, 
-    expect, 
-    it, 
-    render, 
-    screen, 
-    userEvent, 
-    vi, 
-} from "../../test/index";
+import {
+    afterEach,
+    describe,
+    expect,
+    it,
+    render,
+    screen,
+    userEvent,
+    vi,
+} from "../../test";
 
 import { Sidebar } from "./index";
 import { cards } from "../../data/cards";
 
-describe( "component Sidebar", () => {
-    it( "should call function setDeck when button Reset is clicked", async () => {
-        const sortedCards = cards;
-        const setDeck = vi.fn();
+const setDeck = vi.fn();
 
-        render(
-            <Sidebar
-                cards={ sortedCards }
-                setDeck={ setDeck }
-            />
-        );
+function customRender() {
+    const sortedCards = cards;
+    /* const setDeck = vi.fn(); */
+
+    render(
+        <Sidebar
+            cards={ sortedCards }
+            setDeck={ setDeck }
+        />
+    );
+}
+
+describe( "component Sidebar", () => {
+    afterEach( () => {
+        vi.clearAllMocks();
+    } );
+
+    it( "should call function setDeck when button Reset is clicked", async () => {
+        customRender();
+
+        const user = userEvent.setup();
 
         const buttonReset = screen.getByRole( "button", { name: /reset/i } );
-        await userEvent.click( buttonReset );
+        await user.click( buttonReset );
 
         expect( setDeck ).toHaveBeenCalledOnce();
         expect( setDeck ).toHaveBeenCalledTimes( 1 );
     } );
 
     it( "should focus Reset button when tab", async () => {
-        const sortedCards = cards;
-        const setDeck = vi.fn();
+        customRender();
 
-        render(
-            <Sidebar
-                cards={ sortedCards }
-                setDeck={ setDeck }
-            />
-        );
+        const user = userEvent.setup();
 
         const buttonReset = screen.getByRole( "button", { name: /reset/i } );
-        await userEvent.tab();
+        await user.tab();
 
         expect( buttonReset ).toHaveFocus();
     } );
 
     it( "should call function setDeck when press key Enter on button Reset focused ", async () => {
-        const sortedCards = cards;
-        const setDeck = vi.fn();
+        customRender();
 
-        render(
-            <Sidebar
-                cards={ sortedCards }
-                setDeck={ setDeck }
-            />
-        );
-        
-        await userEvent.tab();
-        await userEvent.keyboard( "[Enter]" );
+        const user = userEvent.setup();
+
+        await user.tab();
+        await user.keyboard( "[Enter]" );
 
         expect( setDeck ).toHaveBeenCalledOnce();
         expect( setDeck ).toHaveBeenCalledTimes( 1 );
